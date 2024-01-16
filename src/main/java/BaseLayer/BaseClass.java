78ea74d8bff5e7f025604a4ff6201e01cd31f043
ruchazaware <ruchazavare19@gmail.com>
2024-01-16 15:07:56 +0530

    LoginPage Created

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -1,5 +1,42 @@
 package BaseLayer;
 
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public BaseClass() {
+
+		prop = new Properties();
+
+		try {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "//src//main/java//ConfigurationLayer//config.properties");
+			prop.load(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void initialization() {
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+		driver.manage().deleteAllCookies();
+
+		driver.get(prop.getProperty("url"));
+	}
 
 }
--- src/test/java/Steps/LoginPageStep.java
+++ src/test/java/Steps/LoginPageStep.java
@@ -1,5 +1,33 @@
 package Steps;
 
-public class LoginPageStep {
+import org.testng.Assert;
+
+import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStep extends BaseClass{
+	
+	LoginPage login;
+	
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	   BaseClass.initialization();
+	}
+
+	@When("user enter valid email and password")
+	public void user_enter_valid_email_and_password() {
+	 LoginPage login = new LoginPage();
+	 login.loginFunctionality("Admin","admin123");
+	}
+
+	@Then("user is on home page and validate user url")
+	public void user_is_on_home_page_and_validate_user_url() {
+	   String url =  driver.getCurrentUrl();
+	  boolean a =  url.contains("dashboard");
+	  Assert.assertEquals(a, true);
+	}
 
 }
